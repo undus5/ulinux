d46ad49f4a99c9636a532cdb5727b17e6aa407a9
undus5 <mail@undus.net>
2026-07-13 17:18:17 +0800

    minor update

--- uboot/root.d/usr/local/bin/dracut-install.sh
+++ uboot/root.d/usr/local/bin/dracut-install.sh
@@ -11,29 +11,29 @@ KIMG=${KDIR}/${KVER}/vmlinuz # fedora, archlinux
 [[ -f $KIMG ]] || KIMG=/boot/vmlinuz-${KVER} # ubuntu
 [[ -f $KIMG ]] || exit 1
 
-install -Dm0644 $KIMG ${DEST}/vmlinuz
-echo "==> installed '${DEST}/vmlinuz' (${KVER})"
-echo "==> generating initramfs.img ..."
 if [[ -f /etc/dracut.conf.d/10-dmsquash-live.conf ]]; then
- # for live ISO
- dracut --force --no-hostonly \
-    --kver "$KVER" "${DEST}/initramfs.img"
+   echo "==> generating initramfs.img (live) ..."
+   dracut --force --no-hostonly \
+      --kver "$KVER" "${DEST}/initramfs.img"
 else
- dracut --force --hostonly --no-hostonly-cmdline \
-    --kver "$KVER" "${DEST}/initramfs.img"
+   echo "==> generating initramfs.img ..."
+   dracut --force --hostonly --no-hostonly-cmdline \
+      --kver "$KVER" "${DEST}/initramfs.img"
 fi
 echo "==> installed '${DEST}/initramfs.img' (${KVER})"
+install -Dm0644 $KIMG ${DEST}/vmlinuz
+echo "==> installed '${DEST}/vmlinuz' (${KVER})"
 
 if ! findmnt /a &>/dev/null; then
-   install -Dm0644 ${DEST}/vmlinuz /efi/a/vmlinuz
-   echo "==> installed '/efi/a/vmlinuz' (${KVER})"
    install -Dm0644 ${DEST}/initramfs.img /efi/a/initramfs.img
    echo "==> installed '/efi/a/initramfs.img' (${KVER})"
+   install -Dm0644 ${DEST}/vmlinuz /efi/a/vmlinuz
+   echo "==> installed '/efi/a/vmlinuz' (${KVER})"
 fi
 
 if ! findmnt /b &>/dev/null; then
-   install -Dm0644 ${DEST}/vmlinuz /efi/b/vmlinuz
-   echo "==> installed '/efi/b/vmlinuz' (${KVER})"
    install -Dm0644 ${DEST}/initramfs.img /efi/b/initramfs.img
    echo "==> installed '/efi/b/initramfs.img' (${KVER})"
+   install -Dm0644 ${DEST}/vmlinuz /efi/b/vmlinuz
+   echo "==> installed '/efi/b/vmlinuz' (${KVER})"
 fi
